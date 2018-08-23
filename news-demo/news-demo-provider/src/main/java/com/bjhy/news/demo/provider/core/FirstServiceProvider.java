6f2ee2537f013a01275c78087bd184561967b3cd
wulin-challenge <1178649872@qq.com>
2018-08-23 21:26:40 +0800

    完善demo

--- news-demo/news-demo-provider/src/main/java/com/bjhy/news/demo/provider/core/FirstServiceProvider.java
+++ news-demo/news-demo-provider/src/main/java/com/bjhy/news/demo/provider/core/FirstServiceProvider.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.stereotype.Service;
 
 import com.bjhy.news.common.annotation.NewsListenerService;
+import com.bjhy.news.common.util.NewsRpcUtil;
 import com.bjhy.news.demo.api.FirstService;
 import com.bjhy.news.demo.domain.User;
 
@@ -20,7 +21,7 @@ public class FirstServiceProvider implements FirstService{
 	@Override
 	public String hello1(String str) throws Exception{
 //		System.out.println("测试异常情况下是否重试"+str);
-//		int i = 1/0;
+		
 		String s = "first hello1 "+str;
 		System.out.println(s);
 		return s;
@@ -28,8 +29,9 @@ public class FirstServiceProvider implements FirstService{
 	
 	@Override
 	public String hello2(String str1, String str2, String str3) throws Exception {
-		String str = "hello 2 : "+str1+","+str2+","+str3;
+		String str = "hello 2 : "+str1+","+str2+","+str3+NewsRpcUtil.getPid();
 		System.out.println("write: "+str);
+//		int i = 1/0;
 		return "return : "+str;
 	}
 
